e827373d0848e0f58608142093ee4267591fa518
Graze <me@graze.cc>
2025-06-07 14:46:43 +0100

    wawa rename factor

--- CosmeticRe-Textureizer/Plugin.cs
+++ CosmeticRe-Textureizer/Plugin.cs
@@ -4,91 +4,99 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using BepInEx;
 using HarmonyLib;
 using UnityEngine;
 
-namespace CosmeticRe_Textureizer
+namespace Cosmetic_ReTextureizer
 {
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        public static Dictionary<string, Texture> TextureList = new Dictionary<string, Texture>();
-        static List<GameObject> ReTexturedGameObjects = new List<GameObject>();
-        static List<GameObject[]> ThingsToSearch = new List<GameObject[]>();
-        static string CurrentPath, TexturePath;
-        Action asyncTextureFind;
+        private static Dictionary<string, Texture> _textureList = new();
+        static List<GameObject[]> _thingsToSearch = new();
+        private static string? _currentPath, _texturePath;
         Plugin()
         {
             new Harmony(PluginInfo.GUID).PatchAll(Assembly.GetExecutingAssembly());
-            CurrentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            TexturePath = Path.Combine(CurrentPath, "CosmeticTetxures");
+            ThreadingHelper.Instance.StartAsyncInvoke(() => GetTextures);
         }
 
         void Start()
         {
-            if (!Directory.Exists(TexturePath))
+            _currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (_currentPath != null) _texturePath = Path.Combine(_currentPath, "CosmeticTetxures");
+
+            if (!Directory.Exists(_texturePath))
             {
-                Directory.CreateDirectory(TexturePath);
+                if (_texturePath != null) Directory.CreateDirectory(_texturePath);
             }
-            asyncTextureFind += GetTextures;
-            ThreadingHelper.Instance.StartAsyncInvoke(()=>asyncTextureFind);
             CosmeticsV2Spawner_Dirty.OnPostInstantiateAllPrefabs2 += Run;
         }
         void GetTextures()
         {
-            foreach (var texture in Directory.GetFiles(TexturePath))
-            {
-                Texture2D tex = new Texture2D(2, 2);
-                var imgdata = File.ReadAllBytes(texture);
-                tex.LoadImage(imgdata);
-                string name = Path.GetFileNameWithoutExtension(texture);
-                tex.name = name;
-                tex.filterMode = FilterMode.Point;
-                if (!TextureList.Keys.Contains(name))
+            if (_texturePath != null)
+                foreach (var texture in Directory.GetFiles(_texturePath))
                 {
-                    TextureList.Add(name, tex);
+                    Texture2D tex = new Texture2D(2, 2);
+                    var imgdata = File.ReadAllBytes(texture);
+                    tex.LoadImage(imgdata);
+                    string fileNane = Path.GetFileNameWithoutExtension(texture);
+                    tex.name = fileNane;
+                    tex.filterMode = FilterMode.Point;
+                    if (!_textureList.Keys.Contains(fileNane))
+                    {
+                        _textureList.Add(fileNane, tex);
+                    }
                 }
-            }
         }
 
         void Run()
         {
-            ThingsToSearch.Add(GorillaTagger.Instance.offlineVRRig.cosmetics);
-            ThingsToSearch.Add(GorillaTagger.Instance.offlineVRRig.overrideCosmetics);
-            foreach (RigContainer rigC in Resources.FindObjectsOfTypeAll<RigContainer>())
+            _thingsToSearch.Add(GorillaTagger.Instance.offlineVRRig.cosmetics);
+            _thingsToSearch.Add(GorillaTagger.Instance.offlineVRRig.overrideCosmetics);
+            foreach (RigContainer rigC in VRRigCache.freeRigs)
             {
-                ThingsToSearch.Add(rigC.Rig.cosmetics);
+                _thingsToSearch.Add(rigC.Rig.cosmetics);
             }
-            StartCoroutine(FindTexturesToApply());
+            ThreadingHelper.Instance.StartAsyncInvoke(() => StartTextureApply);
         }
 
-        static IEnumerator FindTexturesToApply()
+        void StartTextureApply() =>
+            StartCoroutine(FindTexturesToApply());
+
+        IEnumerator FindTexturesToApply()
         {
-            foreach (GameObject[] things in ThingsToSearch)
+            try
             {
-                foreach (GameObject cosG in things)
+                foreach (GameObject[] things in _thingsToSearch)
                 {
-                    if (TextureList.ContainsKey(cosG.name))
+                    foreach (GameObject cosG in things)
                     {
-                        if (cosG.GetComponent<Renderer>() != null)
+                        if (_textureList.ContainsKey(cosG.name))
                         {
-                            cosG.GetComponent<Renderer>().material.EnableKeyword("_USE_TEXTURE");
-                            cosG.GetComponent<Renderer>().material.DisableKeyword("_USE_TEX_ARRAY_ATLAS");
-                            cosG.GetComponent<Renderer>().material.mainTexture = TextureList[cosG.name];
+                            if (cosG.GetComponent<Renderer>() != null)
+                            {
+                                cosG.GetComponent<Renderer>().material.EnableKeyword("_USE_TEXTURE");
+                                cosG.GetComponent<Renderer>().material.DisableKeyword("_USE_TEX_ARRAY_ATLAS");
+                                cosG.GetComponent<Renderer>().material.mainTexture = _textureList[cosG.name];
+                            }
+                            foreach (var r in cosG.GetComponentsInChildren<Renderer>())
+                            {
+                                r.material.EnableKeyword("_USE_TEXTURE");
+                                r.material.DisableKeyword("_USE_TEX_ARRAY_ATLAS");
+                                r.material.mainTexture = _textureList[cosG.name];
+                            }
                         }
-                        foreach (var r in cosG.GetComponentsInChildren<Renderer>())
-                        {
-                            r.material.EnableKeyword("_USE_TEXTURE");
-                            r.material.DisableKeyword("_USE_TEX_ARRAY_ATLAS");
-                            r.material.mainTexture = TextureList[cosG.name];
-                        }
-                        ReTexturedGameObjects.Add(cosG);
                     }
                 }
             }
-            yield return "WAWA";
+            catch (Exception exception)
+            {
+                Logger.LogError($"IE-FindTexturesToApply::{exception.Message + exception.StackTrace}");
+            }
+
+            yield return "WaWa";
         }
     }
 }
--- CosmeticRe-Textureizer/PluginInfo.cs
+++ CosmeticRe-Textureizer/PluginInfo.cs
@@ -1,12 +1,9 @@
-﻿namespace CosmeticRe_Textureizer
+﻿namespace Cosmetic_ReTextureizer
 {
-	/// <summary>
-	/// This class is used to provide information about your mod to BepInEx.
-	/// </summary>
 	class PluginInfo
 	{
-		public const string GUID = "com.graze.gorillatag.cosmeticre-textureizer";
-		public const string Name = "CosmeticRe_Textureizer";
+		public const string GUID = "Graze.Cosmetic.ReTextureizer";
+		public const string Name = "Cosmetic ReTextureizer";
 		public const string Version = "1.0.0";
 	}
 }
